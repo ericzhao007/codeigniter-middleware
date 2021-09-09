d08fae849681c06269eb40f534f20ce731ef5045
zhaoxinwei <zhaoxinwei@smzdm.com>
2021-09-09 13:33:52 +0800

    add middleware abstract

---
+++ src/abstracts/CI_Middleware.php
@@ -0,0 +1,38 @@
+<?php
+namespace Codeigniter\Middleware\Abstracts;
+
+/**
+ * 中间件基类
+ *
+ * @Author zhaoxinwei
+ * @DateTime 2021-08-26 17:20
+ */
+abstract class CI_Middleware
+{
+    /**
+     * Constructor
+     *
+     * @access public
+     */
+    function __construct()
+    {
+        log_message('debug', "middleware Class Initialized");
+    }
+
+    abstract public function handle(\Closure $next);
+
+    /**
+     * __get
+     *
+     * Allows models to access CI's loaded classes using the same
+     * syntax as controllers.
+     *
+     * @param	string
+     * @access private
+     */
+    function __get($key)
+    {
+        $CI =& get_instance();
+        return $CI->$key;
+    }
+}
